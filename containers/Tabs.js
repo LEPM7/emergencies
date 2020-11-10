55ef4cd2445921e318123c8729a49711c4cf7938
Luis Peralta <luiseduardoperaltamartinez@gmail.com>
2020-11-10 17:01:23 -0600

    add tabs

--- containers/Tabs.js
+++ containers/Tabs.js
@@ -1,13 +1,19 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
 import TestComponent from '../components/TestComponent';
 
 const Tab = createMaterialTopTabNavigator();
 
-function MyTabs() {
+export default function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Hola" component={TestComponent} />
-      <Tab.Screen name="Adios" component={TestComponent} />
-    </Tab.Navigator>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Hola" component={TestComponent} />
+        <Tab.Screen name="Adios" component={TestComponent} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
